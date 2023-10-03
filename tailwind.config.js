2aacc2b878458aff13730b1c8efdd1454d159a9d
codeConqueror7 <143751280+codeConqueror7@users.noreply.github.com>
2023-10-03 18:09:19 -0400

    projeto finalizado feito por codeConqueror7

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.html", "./src/**/*.{html,js}"],
+ // ...
+
   darkMode:'class',
   theme: {
     extend: {
